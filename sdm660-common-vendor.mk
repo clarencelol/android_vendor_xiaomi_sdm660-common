b4ea9451dad066b9a16886e02803d332ddb12d3b
clarencelol <clarencekuiek@icloud.com>
2021-06-08 12:35:57 +0800

    sdm660-common: Drop libgpustats
    Signed-off-by: clarencelol <clarencekuiek@icloud.com>

--- sdm660-common-vendor.mk
+++ sdm660-common-vendor.mk
@@ -814,7 +814,6 @@ PRODUCT_COPY_FILES += \
     vendor/xiaomi/sdm660-common/proprietary/vendor/radio/qcril_database/upgrade/7_version_update_ecc_table.sql:$(TARGET_COPY_OUT_VENDOR)/radio/qcril_database/upgrade/7_version_update_ecc_table.sql
 
 PRODUCT_PACKAGES += \
-    libgpustats \
     libsdm-disp-vndapis \
     libthermalclient \
     libtime_genoff \
